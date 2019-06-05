2c38a8a45010f1fd29d3947e3932601eed7c0160
Leonardo Rodrigues <leoonardodf@gmail.com>
2019-06-05 11:22:37 -0300

    Arquivos de memória

--- Memory/UserCodeBlock.v
+++ Memory/UserCodeBlock.v
@@ -36,70 +36,78 @@
 // synopsys translate_off
 `timescale 1 ps / 1 ps
 // synopsys translate_on
-module UserCodeBlock (address, byteena, clock, data, rden, wren, q);
-input	[13:0]  address;
-input	[3:0]   byteena;
-input	        clock;
-input	[63:0]  data;
-input	        rden;
-input	        wren;
-output	[63:0]  q;
+module UserCodeBlock (
+	address,
+	byteena,
+	clock,
+	data,
+	rden,
+	wren,
+	q);
 
+	input	[13:0]  address;
+	input	[3:0]  byteena;
+	input	  clock;
+	input	[31:0]  data;
+	input	  rden;
+	input	  wren;
+	output	[31:0]  q;
 `ifndef ALTERA_RESERVED_QIS
-    // synopsys translate_off
+// synopsys translate_off
 `endif
-	tri1	[3:0]   byteena;
-	tri1	        clock;
-	tri1	        rden;
+	tri1	[3:0]  byteena;
+	tri1	  clock;
+	tri1	  rden;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_on
 `endif
 
-wire [63:0] sub_wire0;
-wire [63:0] q = sub_wire0[63:0];
+	wire [31:0] sub_wire0;
+	wire [31:0] q = sub_wire0[31:0];
 
-altsyncram	altsyncram_component (
-    .address_a (address),
-    .byteena_a (byteena),
-    .clock0 (clock),
-    .data_a (data),
-    .rden_a (rden),
-    .wren_a (wren),
-    .q_a (sub_wire0),
-    .aclr0 (1'b0),
-    .aclr1 (1'b0),
-    .address_b (1'b1),
-    .addressstall_a (1'b0),
-    .addressstall_b (1'b0),
-    .byteena_b (1'b1),
-    .clock1 (1'b1),
-    .clocken0 (1'b1),
-    .clocken1 (1'b1),
-    .clocken2 (1'b1),
-    .clocken3 (1'b1),
-    .data_b (1'b1),
-    .eccstatus (),
-    .q_b (),
-    .rden_b (1'b1),
-    .wren_b (1'b0)
-);
-defparam
-    altsyncram_component.byte_size = 8,
-    altsyncram_component.clock_enable_input_a = "BYPASS",
-    altsyncram_component.clock_enable_output_a = "BYPASS",
-    altsyncram_component.init_file = "de1_text.mif",
-    altsyncram_component.intended_device_family = "Cyclone V",
-    altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=TEXT",
-    altsyncram_component.lpm_type = "altsyncram",
-    altsyncram_component.numwords_a = 16384,
-    altsyncram_component.operation_mode = "SINGLE_PORT",
-    altsyncram_component.outdata_aclr_a = "NONE",
-    altsyncram_component.outdata_reg_a = "UNREGISTERED",
-    altsyncram_component.power_up_uninitialized = "FALSE",
-    altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ",
-    altsyncram_component.widthad_a = 14,
-    altsyncram_component.width_a = 32,
-    altsyncram_component.width_byteena_a = 4;
+	altsyncram	altsyncram_component (
+				.address_a (address),
+				.byteena_a (byteena),
+				.clock0 (clock),
+				.data_a (data),
+				.rden_a (rden),
+				.wren_a (wren),
+				.q_a (sub_wire0),
+				.aclr0 (1'b0),
+				.aclr1 (1'b0),
+				.address_b (1'b1),
+				.addressstall_a (1'b0),
+				.addressstall_b (1'b0),
+				.byteena_b (1'b1),
+				.clock1 (1'b1),
+				.clocken0 (1'b1),
+				.clocken1 (1'b1),
+				.clocken2 (1'b1),
+				.clocken3 (1'b1),
+				.data_b (1'b1),
+				.eccstatus (),
+				.q_b (),
+				.rden_b (1'b1),
+				.wren_b (1'b0));
+	defparam
+		altsyncram_component.byte_size = 8,
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.init_file = "de1_text.mif",
+		altsyncram_component.intended_device_family = "Cyclone V",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=TEXT",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 16384,
+		altsyncram_component.operation_mode = "SINGLE_PORT",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "UNREGISTERED",
+		altsyncram_component.power_up_uninitialized = "FALSE",
+		altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ",
+		altsyncram_component.widthad_a = 14,
+		altsyncram_component.width_a = 32,
+		altsyncram_component.width_byteena_a = 4;
+
+
 endmodule
 
 // ============================================================
--- Memory/UserDataBlock.v
+++ Memory/UserDataBlock.v
@@ -36,70 +36,78 @@
 // synopsys translate_off
 `timescale 1 ps / 1 ps
 // synopsys translate_on
-module UserDataBlock (address, byteena, clock, data, rden, wren, q);
+module UserDataBlock (
+	address,
+	byteena,
+	clock,
+	data,
+	rden,
+	wren,
+	q);
 
-input	[16:0]  address;
-input	[3:0]   byteena;
-input	        clock;
-input	[63:0]  data;
-input	        rden;
-input	        wren;
-output	[63:0]  q;
+	input	[13:0]  address;
+	input	[7:0]  byteena;
+	input	  clock;
+	input	[63:0]  data;
+	input	  rden;
+	input	  wren;
+	output	[63:0]  q;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_off
 `endif
-	tri1	[3:0]   byteena;
-	tri1	        clock;
-	tri1	        rden;
+	tri1	[7:0]  byteena;
+	tri1	  clock;
+	tri1	  rden;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_on
 `endif
 
-wire [63:0] sub_wire0;
-wire [63:0] q = sub_wire0[63:0];
+	wire [63:0] sub_wire0;
+	wire [63:0] q = sub_wire0[63:0];
 
-altsyncram	altsyncram_component (
-    .address_a (address),
-    .byteena_a (byteena),
-    .clock0 (clock),
-    .data_a (data),
-    .rden_a (rden),
-    .wren_a (wren),
-    .q_a (sub_wire0),
-    .aclr0 (1'b0),
-    .aclr1 (1'b0),
-    .address_b (1'b1),
-    .addressstall_a (1'b0),
-    .addressstall_b (1'b0),
-    .byteena_b (1'b1),
-    .clock1 (1'b1),
-    .clocken0 (1'b1),
-    .clocken1 (1'b1),
-    .clocken2 (1'b1),
-    .clocken3 (1'b1),
-    .data_b (1'b1),
-    .eccstatus (),
-    .q_b (),
-    .rden_b (1'b1),
-    .wren_b (1'b0)
-);
-defparam
-    altsyncram_component.byte_size = 8,
-    altsyncram_component.clock_enable_input_a = "BYPASS",
-    altsyncram_component.clock_enable_output_a = "BYPASS",
-    altsyncram_component.init_file = "de1_data.mif",
-    altsyncram_component.intended_device_family = "Cyclone V",
-    altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=DATA",
-    altsyncram_component.lpm_type = "altsyncram",
-    altsyncram_component.numwords_a = 131072,
-    altsyncram_component.operation_mode = "SINGLE_PORT",
-    altsyncram_component.outdata_aclr_a = "NONE",
-    altsyncram_component.outdata_reg_a = "UNREGISTERED",
-    altsyncram_component.power_up_uninitialized = "FALSE",
-    altsyncram_component.read_during_write_mode_port_a = "DONT_CARE",
-    altsyncram_component.widthad_a = 16,
-    altsyncram_component.width_a = 64,
-    altsyncram_component.width_byteena_a = 4;
+	altsyncram	altsyncram_component (
+				.address_a (address),
+				.byteena_a (byteena),
+				.clock0 (clock),
+				.data_a (data),
+				.rden_a (rden),
+				.wren_a (wren),
+				.q_a (sub_wire0),
+				.aclr0 (1'b0),
+				.aclr1 (1'b0),
+				.address_b (1'b1),
+				.addressstall_a (1'b0),
+				.addressstall_b (1'b0),
+				.byteena_b (1'b1),
+				.clock1 (1'b1),
+				.clocken0 (1'b1),
+				.clocken1 (1'b1),
+				.clocken2 (1'b1),
+				.clocken3 (1'b1),
+				.data_b (1'b1),
+				.eccstatus (),
+				.q_b (),
+				.rden_b (1'b1),
+				.wren_b (1'b0));
+	defparam
+		altsyncram_component.byte_size = 8,
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.init_file = "de1_data.mif",
+		altsyncram_component.intended_device_family = "Cyclone V",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=DATA",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 16384,
+		altsyncram_component.operation_mode = "SINGLE_PORT",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "UNREGISTERED",
+		altsyncram_component.power_up_uninitialized = "FALSE",
+		altsyncram_component.read_during_write_mode_port_a = "DONT_CARE",
+		altsyncram_component.widthad_a = 14,
+		altsyncram_component.width_a = 64,
+		altsyncram_component.width_byteena_a = 8;
+
+
 endmodule
 
 // ============================================================
@@ -125,7 +133,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ID STRING "DATA"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 // Retrieval info: PRIVATE: MIFfilename STRING "de1_data.mif"
-// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "32768"
+// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "16384"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: READ_DURING_WRITE_MODE_PORT_A NUMERIC "2"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -135,8 +143,8 @@ endmodule
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "1"
 // Retrieval info: PRIVATE: WRCONTROL_ACLR_A NUMERIC "0"
-// Retrieval info: PRIVATE: WidthAddr NUMERIC "15"
-// Retrieval info: PRIVATE: WidthData NUMERIC "32"
+// Retrieval info: PRIVATE: WidthAddr NUMERIC "14"
+// Retrieval info: PRIVATE: WidthData NUMERIC "64"
 // Retrieval info: PRIVATE: rden NUMERIC "1"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: BYTE_SIZE NUMERIC "8"
@@ -146,29 +154,29 @@ endmodule
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=DATA"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
-// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "32768"
+// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "16384"
 // Retrieval info: CONSTANT: OPERATION_MODE STRING "SINGLE_PORT"
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
 // Retrieval info: CONSTANT: POWER_UP_UNINITIALIZED STRING "FALSE"
 // Retrieval info: CONSTANT: READ_DURING_WRITE_MODE_PORT_A STRING "DONT_CARE"
-// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "15"
-// Retrieval info: CONSTANT: WIDTH_A NUMERIC "32"
-// Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "4"
-// Retrieval info: USED_PORT: address 0 0 15 0 INPUT NODEFVAL "address[14..0]"
-// Retrieval info: USED_PORT: byteena 0 0 4 0 INPUT VCC "byteena[3..0]"
+// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "14"
+// Retrieval info: CONSTANT: WIDTH_A NUMERIC "64"
+// Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "8"
+// Retrieval info: USED_PORT: address 0 0 14 0 INPUT NODEFVAL "address[13..0]"
+// Retrieval info: USED_PORT: byteena 0 0 8 0 INPUT VCC "byteena[7..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
-// Retrieval info: USED_PORT: data 0 0 32 0 INPUT NODEFVAL "data[31..0]"
-// Retrieval info: USED_PORT: q 0 0 32 0 OUTPUT NODEFVAL "q[31..0]"
+// Retrieval info: USED_PORT: data 0 0 64 0 INPUT NODEFVAL "data[63..0]"
+// Retrieval info: USED_PORT: q 0 0 64 0 OUTPUT NODEFVAL "q[63..0]"
 // Retrieval info: USED_PORT: rden 0 0 0 0 INPUT VCC "rden"
 // Retrieval info: USED_PORT: wren 0 0 0 0 INPUT NODEFVAL "wren"
-// Retrieval info: CONNECT: @address_a 0 0 15 0 address 0 0 15 0
-// Retrieval info: CONNECT: @byteena_a 0 0 4 0 byteena 0 0 4 0
+// Retrieval info: CONNECT: @address_a 0 0 14 0 address 0 0 14 0
+// Retrieval info: CONNECT: @byteena_a 0 0 8 0 byteena 0 0 8 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
-// Retrieval info: CONNECT: @data_a 0 0 32 0 data 0 0 32 0
+// Retrieval info: CONNECT: @data_a 0 0 64 0 data 0 0 64 0
 // Retrieval info: CONNECT: @rden_a 0 0 0 0 rden 0 0 0 0
 // Retrieval info: CONNECT: @wren_a 0 0 0 0 wren 0 0 0 0
-// Retrieval info: CONNECT: q 0 0 32 0 @q_a 0 0 32 0
+// Retrieval info: CONNECT: q 0 0 64 0 @q_a 0 0 64 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL UserDataBlock.v TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL UserDataBlock.inc FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL UserDataBlock.cmp FALSE
